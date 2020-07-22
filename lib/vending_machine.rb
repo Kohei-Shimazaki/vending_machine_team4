19cffc15c1bf21650327e4575fa0aa88fd3d7e17
Kohei-Shimazaki <kohei.shimazaki.k@gmail.com>
2020-07-22 01:46:29 +0000

    コメント追加

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,18 +1,11 @@
 class Drink
-  attr_reader :name, :price, :stock
+  attr_reader :name, :price
+  attr_accessor :stock
 
   def initialize(name, price, stock)
     @name = name
     @price = price
     @stock = stock
-  end
-
-  def sell
-    @stock -= 1
-  end
-
-  def restock(restock)
-    @stock += restock
   end
 end
 
@@ -20,41 +13,43 @@ class VendingMachine
   attr_reader :slot_money, :earnings, :products
   MONEY = [10, 50, 100, 500, 1000].freeze
 
-  def init_products
+  def initialize
+    @slot_money = 0
+    @earnings = 0
     @products = []
     @products.push(Drink.new("コーラ", 120, 5))
     @products.push(Drink.new("レッドブル", 200, 5))
     @products.push(Drink.new("水", 100, 5))
   end
 
+  # 商品を補充するメソッド
   def restock(name, stock)
     if @slot_money == 0
       if drink = @products.find{|product| product.name == name}
-        drink.restock(stock)
+        drink.stock += stock
       else
         puts "#{name}の値段はいくらですか？"
         price = gets.chomp!.to_i
         @products.push(Drink.new(name, price, stock))
       end
+    else
+      puts '投入金を回収してからやり直してください'
     end
   end
 
-  def initialize
-    @slot_money = 0
-    @earnings = 0
-    @products = init_products
-  end
-
+  # 投入金を入れるメソッド
   def slot(money)
     MONEY.include?(money) ? @slot_money += money : puts(money)
     able_to_sell_lists
   end
 
+  # 投入金を返却するレバーのイメージ
   def return_money
     puts "返却：#{@slot_money}円"
     @slot_money = 0
   end
 
+  # 商品ボタンが光るイメージ
   def able_to_sell_lists
     @able_to_sell_lists = []
     @products.each do |product|
@@ -65,24 +60,25 @@ class VendingMachine
     @able_to_sell_lists.each{|product| puts "#{product.name}は購入可能です"}
   end
 
+  # 商品を売るメソッド
   def sell(name)
-    if choose = @able_to_sell_lists.find{|product| product.name == name}
+    if (choose = @able_to_sell_lists.find{|product| product.name == name})
       @earnings += choose.price
       @slot_money -= choose.price
-      choose.sell
+      choose.stock -= 1 #在庫を1減らす
       puts "購入：#{choose.name}, 釣り銭：#{@slot_money}円"
     end
   end
 end
 
-#vm = VendingMachine.new
-#vm.slot(100)
-#vm.slot(10)
-#vm.slot(10)
-#p vm.able_to_sell_lists
+vm = VendingMachine.new
+vm.slot(100)
+vm.slot(10)
+vm.slot(10)
+
 #vm.restock("コーラ", 5)
 #p vm.able_to_sell_lists
 #vm.sell("サイダー")
-#vm.sell("コーラ")
+vm.sell("コーラ")
 #p vm.products
 #vm.return_money
